90684b841261e88cea01d84388371e1e71fb9b67
Jon Wexler <jwexman@gmail.com>
2021-02-23 17:46:24 -0500

    [main] gitignore and demo

--- 2_express_api/end/index.js
+++ 2_express_api/end/index.js
@@ -18,6 +18,25 @@ const data = {
   }]
 }
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 const router = express.Router();
 app.use(express.json());
 app.use('/v1', router);
@@ -34,7 +53,7 @@ function authMiddleware (req, res, next) {
 
 function productRouter () {
   const productRouter = express.Router();
-  productRouter.use(authMiddleware);
+  // productRouter.use(authMiddleware);
   productRouter.get('/', (req, res) => {
     res.status(200).send(data.products)
   })
